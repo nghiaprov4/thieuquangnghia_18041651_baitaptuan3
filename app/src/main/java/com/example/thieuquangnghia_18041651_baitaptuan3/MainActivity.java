07139ab1921ad99028d1baee91e0efaec3d6c8d9
DESKTOP-41VA80C\IBM <thieuquangnghia2000@gmail.com>
2021-09-24 15:50:49 +0700

    firstcommit

---
+++ app/src/main/java/com/example/thieuquangnghia_18041651_baitaptuan3/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.thieuquangnghia_18041651_baitaptuan3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    Button buttongo;
+    ImageView imagehoa1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.buttongo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,screen2.class));
+                overridePendingTransition(R.anim.enter,R.anim.exit);
+            }
+        });
+
+    }
+}
